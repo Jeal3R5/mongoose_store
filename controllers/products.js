dd1efb3263f432843d245e004180d3de699091c5
Joan Renfroe <joan.renfroe@gmail.com>
2022-04-19 12:08:38 -0700

    update schema

--- controllers/products.js
+++ controllers/products.js
@@ -1,92 +1,92 @@
-const express = require('express');
-const product = require('.models/products');
-const productSeed = require('./models/productSeed');
-const productRouter = express.Router();
+// const express = require('express');
+// const product = require('.models/products');
+// const productSeed = require('./models/productSeed');
+// const productRouter = express.Router();
 
-productRouter.get('/seed', (req,res) => {
-    product.deleteMany({}, (err, deletedProducts) => {
-        product.create(productSeed, (err, data) => {
-            res.redirect('/products');
-        });
-    });
-});
-
-
-
-
-//Index Route
-productRouter.get('/', (req, res) => {
-    product.find({}, (err, allProducts) => {
-        res.render('index.ejs', {products: allProducts});
-    });
-});
+// productRouter.get('/seed', (req,res) => {
+//     product.deleteMany({}, (err, deletedProducts) => {
+//         product.create(productSeed, (err, data) => {
+//             res.redirect('/products');
+//         });
+//     });
+// });
 
 
 
 
-//New Route
-productRouter.get('/new', (req, res) => {
-    res.render('new.ejs');
-});
+// //Index Route
+// productRouter.get('/', (req, res) => {
+//     product.find({}, (err, allProducts) => {
+//         res.render('index.ejs', {products: allProducts});
+//     });
+// });
 
 
 
 
-//Delete Route
-productRouter.delete('/:id', (req, res) => {
-    product.findByIdAndDelete(req.params.id, (err, deletedProduct) => {
-        res.redirect('/products');
-    });
-});
+// //New Route
+// productRouter.get('/new', (req, res) => {
+//     res.render('new.ejs');
+// });
 
 
 
 
-// Update Route
-productRouter.put('/:id', (req, res) => {
-    console.log(req.body);
-    product.findByIdAndUpdate(req.params.id, req.body, (err, updatedProduct) => {
-        if (err) console.log(err);
-        res.redirect(`/products/${req.params.id}`);
-    });
-});
+// //Delete Route
+// productRouter.delete('/:id', (req, res) => {
+//     product.findByIdAndDelete(req.params.id, (err, deletedProduct) => {
+//         res.redirect('/products');
+//     });
+// });
 
 
 
 
-//Create Route
-productRouter.post('/', (req, res) =>{
-    product.create(req.body, (err, createdProduct) => {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else {
-            res.redirect('/products');
-        }
-    });
-})
+// // Update Route
+// productRouter.put('/:id', (req, res) => {
+//     console.log(req.body);
+//     product.findByIdAndUpdate(req.params.id, req.body, (err, updatedProduct) => {
+//         if (err) console.log(err);
+//         res.redirect(`/products/${req.params.id}`);
+//     });
+// });
 
 
 
 
-//Edit Route
-productRouter.get('/:id/edit', (req, res) => {
-    product.findById(req.params.id, (err, product) => {
-        res.render('edit.ejs', {product})
-    });
-});
+// //Create Route
+// productRouter.post('/', (req, res) =>{
+//     product.create(req.body, (err, createdProduct) => {
+//         if (err) {
+//             console.log(err);
+//             res.send(err);
+//         } else {
+//             res.redirect('/products');
+//         }
+//     });
+// })
 
 
 
 
-//Show Route
-productRouter.get('/:id', (req, res) => {
-    product.findById(req.params.id, (err, product) => {
-        res.render('show.ejs', {product});
-    });
-});
+// //Edit Route
+// productRouter.get('/:id/edit', (req, res) => {
+//     product.findById(req.params.id, (err, product) => {
+//         res.render('edit.ejs', {product})
+//     });
+// });
 
 
 
 
-module.exports = productRouter;
+// //Show Route
+// productRouter.get('/:id', (req, res) => {
+//     product.findById(req.params.id, (err, product) => {
+//         res.render('show.ejs', {product});
+//     });
+// });
+
+
+
+
+// module.exports = productRouter;
--- server.js
+++ server.js
@@ -1,30 +1,25 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const products = require('./models/product');
-const productsController = require('./controllers/products');
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const products = require("./models/product");
+const productsController = require("./controllers/products");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 // Middleware
-app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'));
-app.use('/products', productsController);
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use("/products", productsController);
 
 mongoose.connect(process.env.DATABASE_URL, {
-    usedNewUrlParser: true,
-    useUnifiedTopology: true,
+  usedNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
-db.on('error', (err) => console.log(err.message + "is mongo not running?"));
-db.on('connected', () => console.log('mongoose connected'));
-db.on('disconnected', () => console.log('mongo disconnected'));
-
-
+db.on("error", (err) => console.log(err.message + "is mongo not running?"));
+db.on("connected", () => console.log("mongoose connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
 
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
